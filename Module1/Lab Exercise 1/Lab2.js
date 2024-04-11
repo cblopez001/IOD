a03e00d5eaa3a41246d295f8ffcbae958bc7084a
cblopez001 <cherakye.lopez@gmail.com>
2024-04-10 20:43:25 -0400

    Lab2 Operations and Name Commits

---
+++ Module1/Lab Exercise 1/Lab2.js
@@ -0,0 +1,42 @@
+//Create 4 functions for the 4 main mathematical operations (-,+,/,*). Return the calculated value and then output it to the screen.
+
+//First I defined a variable called number1 and number2. 
+number1 = 10;
+number2 = 2;
+
+//For the addition operation I defined a function called addNumber. When I called the function, I printed the sum of the two numbers.
+function addNumber(){
+    console.log(number1+number2);
+}
+addNumber();
+
+
+//For the subtraction operation I defined a function called subtractNumber. When I called the function, I printed the difference of the two numbers.
+function subtractNumber(){
+    console.log(number1-number2);
+}
+subtractNumber();
+
+
+//For the multiplication operation I defined a function called multiplyNumber. When I called the function, I printed the product of the two numbers.
+function multiplyNumber(){
+    console.log(number1*number2);
+}
+multiplyNumber();
+
+
+//For the division operation I defined a function called divideNumber. When I called the function, I printed the quotient of the two numbers.
+function divideNumber(){
+    console.log(number1/number2);
+}
+divideNumber();
+
+//For this example, I defined a variable called name. When I called the function, I printed "Hello,<name>!".
+let name= "Cherakye Lopez";
+
+function printName(){
+    console.log("Hello," + name+"!");
+}
+
+//I defined a function called printName. When I called the function, I printed "Hello,
+printName();
