611d530929bbc4f2a1b29896c8fff59aecc2e5b8
cblopez001 <cherakye.lopez@gmail.com>
2024-04-10 23:30:58 -0400

    lab4 section 1 commit

---
+++ Module1/LabExercise4/Lab4.js
@@ -0,0 +1,7 @@
+//Section 1: Create a JSON object for a book wih the following properties: title, description, author and # of pages
+
+const book = {title:"Ivanhoe",description:"Sir Walter Scott's historical romance was published in 1819 and has reigned supreme ever since as he epitome of chilvaric novels.",author:"Sir Walter Scott", pageCount:410}
+
+console.log(book);
+
+
