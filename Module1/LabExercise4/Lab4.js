cb77650094414a95304fc8f333aee374889b643a
cblopez001 <cherakye.lopez@gmail.com>
2024-04-10 23:54:22 -0400

    Section 2 and Section 3 additions to the assingment commit

--- Module1/LabExercise4/Lab4.js
+++ Module1/LabExercise4/Lab4.js
@@ -4,4 +4,13 @@ const book = {title:"Ivanhoe",description:"Sir Walter Scott's historical romance
 
 console.log(book);
 
+//SECTION 2: Try printing these object property values in your console individually and via the whole book object.
+console.log(book.title);
+console.log(book.description);
+console.log(book.author);
+console.log(book.pageCount);
 
+//SECTION 3: Update the description of the book.
+book.description = "Ivanhoe crackles with adventure, from hostage drama inside a besieged castle to a trial by combat to determine the fate of an innocent maiden."
+console.log(book.description);
+console.log(book);
