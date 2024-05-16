36e5554ba6a2d832bcad0371cb0e76dd6a230c97
cblopez001 <cherakye.lopez@gmail.com>
2024-05-15 21:55:15 -0400

    changes to lab 3

--- Module3/JSLab3answers.js
+++ Module3/JSLab3answers.js
@@ -15,10 +15,48 @@ function makeCounter(startFrom=0, increments=0) {
     counter2();
     counter2();
 
-    // Module 3_Lab 3 Question 2
+// Module 3_Lab 3 Question 2
 
-    // Module 3_Lab 3 Question 3
+    /* the order that the messages will be printed is #4,#3,#2,#1 because 100 milliseconds is longer than than 0 milliseconds.*/
+    delayMsg=(msg)=> console.log(`This message will be printed after a delay: ${msg}`)
 
-    // Module 3_Lab 3 Question 4
+    setTimeout(delayMsg,15000, '#5: Delayed by 15000ms');
+    setTimeout(delayMsg, 100, '#1: Delayed by 100ms');
+    setTimeout(delayMsg, 20, '#2: Delayed by 20ms');
+    setTimeout(delayMsg, 0, '#3: Delayed by 0ms');
+    delayMsg('#4: Not delayed at all');
+    clearTimeout(setTimeout(delayMsg,15000));
 
-    // Module 3_Lab 3 Question 5
+// Module 3_Lab 3 Question 3
+
+// Module 3_Lab 3 Question 4
+
+// Module 3_Lab 3 Question 5
+
+// Module 3_Lab 3 Question 6
+    
+// Module 3_Lab 3 Question 7
+
+// Module 3_Lab 3 Question 8
+
+// Module 3_Lab 3 Question 9
+
+function randomDelay(){
+const delay=Math.ceil(Math.random()* 20) *1000;
+return new Promise((resolve, reject) => 
+    setTimeout(
+    delay % 2===0 ? resolve : reject,
+    delay,
+    delay
+    (b= Math.random()*10)
+    )
+);
+}
+
+randomDelay()
+.then((delay, b)=>
+     console.log(
+        'There appears to be a delay of ${delay} ms. This is ${b}, a second argument')
+    )
+    .catch(() =>
+        console.log('Failed to delay'));
