b02cd407a7d05b86ebcc84f3e866fb5f6f78c9a3
cblopez001 <cherakye.lopez@gmail.com>
2024-07-03 13:03:29 -0400

    save changes for lab2.2

---
+++ Module6/Lab2.2/big-cats-app/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import BigCats from './components/BigCats';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Big Cats</h1>
+      <BigCats />
+    </div>
+  );
+}
+
+export default App;
