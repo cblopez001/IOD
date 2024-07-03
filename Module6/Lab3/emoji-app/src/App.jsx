f54dbfd2dd67194d2d4a05b55d6da9f676917c39
cblopez001 <cherakye.lopez@gmail.com>
2024-07-03 10:55:45 -0400

    lab3 commit

---
+++ Module6/Lab3/emoji-app/src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+
+import React from 'react';
+import Emoji from './Emoji';
+import './App.css'; // Import your CSS file for styling (if needed)
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Emoji Mood Changer</h1>
+      <Emoji />
+    </div>
+  );
+}
+
+export default App;
+
